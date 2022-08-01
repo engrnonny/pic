f5ce516ba91e3d58ab19de4c4ba11c6d1e1f7d84
engrnonny <engr.nonny@gmail.com>
2022-08-01 16:31:00 +0100

    ffice - Dell - Started working on login view

--- users/urls.py
+++ users/urls.py
@@ -3,8 +3,11 @@ from users.views import *
 
 urlpatterns = [
     path('register/', register, name='register'),
-    path('login/', login, name='login'),
+    path('login/', login_view, name='login'),
+    path('logout/', logout, name='logout'),
+    path('profile/<slug>/', user_profile, name='user-profile'),
     path('', members_landing_page, name='members-landing-page'),
+
 ]
     # path('<slug:slug>/', user_profile, name='user-profile'),
     # path('company/<slug:slug>/', company_profile, name='company-profile'),
--- users/views.py
+++ users/views.py
@@ -1,4 +1,7 @@
+from django.contrib import messages
+from django.contrib.auth import authenticate, login
 from django.shortcuts import get_list_or_404, get_object_or_404, redirect, render
+from django.urls import reverse
 
 
 # Users registration
@@ -12,11 +15,35 @@ def register(request):
 
 # Members login
 
-def login(request):
+def login_view(request):
     if request.method == 'post':
-        pass
-    else:
-        return render(request, 'users/login.html')
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            next_param = request.POST.get('next')
+            if next_param:
+                url = next_param
+            else:
+                url = reverse('user-profile')
+            return redirect(url)
+
+        else:
+            messages.add_message(request, messages.ERROR, 'Login failed')
+    return render(request, 'users/login.html')
+
+
+# Members logout
+
+def logout(request):
+    return redirect('index')
+
+
+# Logged in user profile page
+
+def user_profile(request):
+    return render(request, 'users/user_profile.html')
 
 
 # Landing page for all registered members
