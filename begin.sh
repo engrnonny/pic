b9f69d296f721149bf7223bae76c861b13aa1dac
engrnonny <engr.nonny@gmail.com>
2022-08-04 00:55:21 +0100

    Home - HP1 - Created bash files to automate beginning and ending a session

---
+++ begin.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/expect
+
+env/Scripts/activate
+
+git pull
+
+pg_restore -h localhost -p 5432 -U postgres -d old_db -v
+backup_db
+
+expect "Password: " { send "Tri3nity.\r" }
+
+python manage.py makemigrations
+
+python manage.py migrate
+
+python manage.py runserver
