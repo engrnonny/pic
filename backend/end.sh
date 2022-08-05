894e1c61e2f3d184d284bc4ecf0cfa895e1dda23
engrnonny <engr.nonny@gmail.com>
2022-08-05 05:19:19 +0100

    Home - HP1 - Docker installation

---
+++ backend/end.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/expect
+
+python manage.py makemigrations
+
+# python manage.py migrate
+
+# pg_dump -h localhost -p 5432 -U postgres -F c -b -v -f backup_db
+
+# expect "Password: " { send "Tri3nity.\r" }
+
+# git add *
+
+# git commit -m "Home - HP1 - Finshed Articles test"
+
+# git push
