e3805dc98350268dce16c9543a7cbda7c173db63
engrnonny <engr.nonny@gmail.com>
2022-08-14 06:13:45 +0100

    Home - HP1 - Getting error with rest_framework

--- backend/end.sh
+++ backend/end.sh
@@ -1,12 +1,12 @@
-#!/usr/bin/expect
+# !/usr/bin/expect
 
-# python manage.py makemigrations
+python manage.py makemigrations
 
-# python manage.py migrate
+python manage.py migrate
 
-# pg_dump -h localhost -p 5432 -U postgres -F c -b -v -f backup_db
+pg_dump -h localhost -p 5432 -U postgres -F c -b -v -f backup_db
 
-# expect "Password: " { send "Tri3nity.\r" }
+expect "Password: " { send "Tri3nity.\r" }
 
 git add *
 
